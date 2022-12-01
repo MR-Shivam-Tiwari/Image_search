a748cd73ba79d8d3505ae243750214743df25edf
shivam <shivamt2023@gmail.com>
2022-12-01 19:16:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [photo, setPhoto] = useState("")
+  const [result, setResult] = useState([])
+
+
+  const changePhoto = () => {
+      axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=GrLthNqqcnWLrEsOwC3D8XoOaxEHHtdkeyRfjF6cQ0I`)
+          .then((response) => {
+              console.log(response.data);
+              setResult(response.data.results);
+          })
+          
+  }
+  return(
+    <>
+    <div className='container'>
+      <h1>React Photo Search</h1>
+      <nav>BookMark</nav>
+      <input type="text" className='form-control' value={photo} onChange={(e) => {
+                    setPhoto(e.target.value)
+                }} placeholder="Search free image" />
+      <button type='submit' onClick={changePhoto}>Search</button>
     </div>
-  );
+    <div className="container">
+                <div className='row'>
+                    {result.map((value) => {
+                        return (
+                            <div className='col'>
+                                    <img className="img" src={value.urls.small} alt='' />
+                            </div>
+                        )
+                    })}
+                </div>
+              
+    </div>
+    </>
+  )
 }
 
 export default App;
